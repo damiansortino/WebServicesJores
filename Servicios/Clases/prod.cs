60e27e8d12fff0191f4feef9f9571477c2d40064
DAMIAN SORTINO <92537867+damiansortino@users.noreply.github.com>
2022-12-20 11:57:31 -0300

    Correcciones

---
+++ Servicios/Clases/prod.cs
@@ -0,0 +1,40 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Clases
+{
+    public class prod
+    {
+        public int ProductoId { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public double precioUnitario { get; set; }
+        public double porcentajeRentabilidad { get; set; }
+        public string talle { get; set; }
+        public string color { get; set; }
+        public string marca { get; set; }
+        public string codigo { get; set; }
+        public double precio { get; set; }
+
+        public prod(producto j)
+        {
+            ProductoId = j.ProductoId;
+            nombre = j.nombre;
+            descripcion = j.descripcion;
+            precioUnitario = j.precioUnitario;
+            porcentajeRentabilidad = j.porcentajeRentabilidad;
+            talle = j.talle;
+            color = j.color;
+            marca = j.marca;
+            codigo = j.codigo;
+            precio = j.precioUnitario + (((j.precioUnitario * j.porcentajeRentabilidad))/100);
+        }
+
+        public prod()
+        {
+        }
+    }
+}
---
+++ Servicios/Controllers/ProductoController.cs
@@ -0,0 +1,51 @@
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Servicios.Controllers
+{
+    public class ProductoController : ApiController
+    {
+        public List<prod> Get()
+        {
+            List<prod> listado = new List<prod>();
+            using (baselaymarEntities db = new baselaymarEntities())
+            {
+                foreach (var item in db.producto)
+                {
+                    if (item.fechaBaja == null)
+                    {
+                        prod nuevoprod = new prod(item);
+                        listado.Add(nuevoprod);
+                    }
+                }
+            }
+            return listado;
+        }
+
+        // GET: api/producto/5
+        public prod Get(int id)
+        {
+            prod devolver = new prod();
+                      
+            using (baselaymarEntities db = new baselaymarEntities())
+            {
+                
+                foreach (var item in db.producto)
+                {
+                    if ((item.fechaBaja == null) && (item.codigo.Contains(id.ToString().Trim())))
+                    {
+                        prod nuevo = new prod(item);
+                        devolver = nuevo;
+                    }
+                }
+            }
+            return devolver;
+        }
+    }
+}
